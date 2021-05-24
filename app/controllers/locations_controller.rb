2975cfb42e697be3b1d51f3542f969f973d5f10e
Kirk Paradis <kirk.paradis@gmail.com>
2021-05-24 17:20:08 +0900

    Generated and added basic methods for location controller

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,31 @@
+class LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+  end
+
+  def show
+    @location = Location.find(location_params)
+  end
+  
+  def new
+    @location = Location.new
+  end
+  
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      flash[:success] = "Location successfully created"
+      redirect_to @location
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+
+  private
+
+  def location_params
+    params.require(:location).permit(:user_id, :name, :address, :price_per_day, :description, :capacity)
+  end
+end

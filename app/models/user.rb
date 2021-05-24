addeb48399368bbb4ab9179012d5497eb0c9586a
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-24 17:01:23 +0900

    Added the has_one_attached :photo to both the User model and the Location model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :locations
   has_many :bookings
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :photo
 end

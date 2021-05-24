74f937c830b34624cdfde0dc6ddac291e953408e
abdul muhaimin <muhai07@outlook.com>
2021-05-24 17:38:32 +0900

    added seeds

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 
+#added GEMS
+gem 'faker'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.2'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+  @user =  User.create!(
+      name: "Doug",
+      description: "Place for rent",
+      email:"someone@example.com",
+      password:"123456",
+      )
+
+5.times do
+    Location.create!(
+      name:Faker::Address.street_name,
+      address:Faker::Address.street_address,
+      price_per_day:rand(100..200),
+      description:Faker::Company.catch_phrase,
+      capacity:rand(5..20),
+      user: @user
+      )
+end
+
+puts "... created #{Location.count} ."
+

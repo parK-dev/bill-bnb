ee51cbc9ed200bf0d617cd1ba4b44f38d1b88792
abdul muhaimin <muhai07@outlook.com>
2021-05-25 15:32:59 +0900

    Added Validations to booking and location

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :location
 
   enum status: { pending: 0, confirmed: 1, rejected: 2 }
+  validates :start_date, :end_date, presence: true
+
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -3,4 +3,6 @@ class Location < ApplicationRecord
   has_many :bookings
 
   has_one_attached :photo
+  validates :name, :address, :price_per_day, :description, :capacity, presence: true
+
 end

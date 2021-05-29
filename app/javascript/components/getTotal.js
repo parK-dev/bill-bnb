81476419b65474332c5bf20bd8b4f5710d3d41b2
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-29 15:36:52 +0900

    Refactored code and added animation transition on show

--- app/javascript/components/getTotal.js
+++ app/javascript/components/getTotal.js
@@ -1,39 +1,62 @@
+
+
 const getTotal = () => {
   let startDateValue;
   let endDateValue;
-  let totalDays;
+  let regex = /(\d*[^-])/g;
+
+// SETS THE TOTAL AMOUNT AND RENDERS IT TO THE BOOKING FORM
+  const setTotal = () => {
+    // CALCULATES DIFFERENCE IN DAYS BETWEEN THE TWO DATES
+    let diffTime = Math.abs(endDateValue - startDateValue);
+    let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+
+    // SELECTES THE TOTAL FIELD
+    let totalPriceElement = document.getElementById('total')
+    let totalDiv = document.getElementsByClassName('total-price-wrapper')
+    let price = totalPriceElement.dataset.price
+
+    // RENDERS THE TOTAL IF THERE'S A START AND END DATE
+    if (startDateValue && endDateValue){
+      totalPriceElement.innerHTML = `$ ${diffDays * price}`
+      console.log(totalDiv)
+      totalDiv[0].style.height = '42px'
+      totalDiv[0].style.transition = 'all .2s ease-in'
+    }
+
+
+  }
+
+
+
   // SELECTS THE BOOKING START DATE INPUT
   if(document.getElementById('booking_start_date')){
     let startDateElement = document.getElementById('booking_start_date');
     startDateElement.addEventListener('change', (e) => {
-      // CONVERT DAY TO NUMBER
-      startDateValue = e.currentTarget.value.split('').slice(-2).join('');
-      // SETS TOTAL DAY DIFFERENCE
-      totalDays = parseInt(endDateValue, 10) - parseInt(startDateValue, 10);
-      // SELECTES THE TOTAL FIELD
-      let totalElement = document.getElementById('total')
-      let price = totalElement.dataset.price
-      if (startDateValue && endDateValue){
-        totalElement.innerHTML = `$ ${totalDays * price}`
-      }
+
+    // SETS NEW DATE OBEJECT WITH THE DATE SELECTED FOR START DATE
+    startDateValue = new Date(e.currentTarget.value.match(regex).join('/'))
+
+    setTotal()
     })
   }
     // SELECTS THE BOOKING END DATE INPUT
   if(document.getElementById('booking_end_date')) {
     let endDateElement = document.getElementById('booking_end_date');
     endDateElement.addEventListener('change', (e) => {
-      // CONVERT DAY TO NUMBER
-      endDateValue = e.currentTarget.value.split('').slice(-2).join('')
-       // SETS TOTAL DAY DIFFERENCE
-      totalDays = parseInt(endDateValue, 10) - parseInt(startDateValue, 10)
-      // SELECTES THE TOTAL FIELD
-      let totalElement = document.getElementById('total')
-      let price = totalElement.dataset.price
-      if (startDateValue && endDateValue){
-        totalElement.innerHTML = `$ ${totalDays * price}`
-      }
+
+    // SETS NEW DATE OBEJECT WITH THE DATE SELECTED FOR END DATE
+    endDateValue = new Date(e.currentTarget.value.match(regex).join('/'))
+
+    setTotal()
     })
   }
 }
 
 export { getTotal }
+
+
+// const date1 = new Date('7/13/2010');
+// const date2 = new Date('12/15/2010');
+

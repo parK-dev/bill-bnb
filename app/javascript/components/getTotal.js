7c62a095bbbe53569037ccc2eb2a5ba1c5b57be7
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-29 14:29:23 +0900

    added comments to code

--- app/javascript/components/getTotal.js
+++ app/javascript/components/getTotal.js
@@ -1,24 +1,16 @@
-
-
-
-
-
-
-
-
-
-
 const getTotal = () => {
   let startDateValue;
   let endDateValue;
   let totalDays;
+  // SELECTS THE BOOKING START DATE INPUT
   if(document.getElementById('booking_start_date')){
-
     let startDateElement = document.getElementById('booking_start_date');
     startDateElement.addEventListener('change', (e) => {
+      // CONVERT DAY TO NUMBER
       startDateValue = e.currentTarget.value.split('').slice(-2).join('');
+      // SETS TOTAL DAY DIFFERENCE
       totalDays = parseInt(endDateValue, 10) - parseInt(startDateValue, 10);
-
+      // SELECTES THE TOTAL FIELD
       let totalElement = document.getElementById('total')
       let price = totalElement.dataset.price
       if (startDateValue && endDateValue){
@@ -26,12 +18,15 @@ const getTotal = () => {
       }
     })
   }
+    // SELECTS THE BOOKING END DATE INPUT
   if(document.getElementById('booking_end_date')) {
     let endDateElement = document.getElementById('booking_end_date');
     endDateElement.addEventListener('change', (e) => {
+      // CONVERT DAY TO NUMBER
       endDateValue = e.currentTarget.value.split('').slice(-2).join('')
+       // SETS TOTAL DAY DIFFERENCE
       totalDays = parseInt(endDateValue, 10) - parseInt(startDateValue, 10)
-
+      // SELECTES THE TOTAL FIELD
       let totalElement = document.getElementById('total')
       let price = totalElement.dataset.price
       if (startDateValue && endDateValue){
